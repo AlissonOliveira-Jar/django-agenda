3e42868359f84fd8ace97fabd38c6e2bfe77f8ff
Alisson Oliveira <alisson.dev.py@gmail.com>
2024-09-04 16:00:57 -0300

    :truck: chore: arquivos movidos para a pasta source

--- source/contact/admin.py
+++ source/contact/admin.py
@@ -11,7 +11,7 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = "id", "first_name", "last_name", "phone"
+    list_display = "id", "first_name", "last_name", "phone", "show"
     ordering = ("-id",)
     list_filter = ("created_date",)
     search_fields = (
@@ -24,6 +24,7 @@ class ContactAdmin(admin.ModelAdmin):
     list_editable = (
         "first_name",
         "last_name",
+        "show",
     )
     list_display_links = (
         "id",
