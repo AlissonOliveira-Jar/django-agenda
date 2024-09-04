00e456c8c156fd0300f6dc93ff5dddded100115a
Alisson Oliveira <alisson.dev.py@gmail.com>
2024-09-04 19:04:12 -0300

    :sparkles: feat: menssagem de sucesso

--- source/contact/forms.py
+++ source/contact/forms.py
@@ -50,7 +50,7 @@ class RegisterForm(UserCreationForm):
     first_name = forms.CharField(required=True, min_length=3)
     last_name = forms.CharField(required=True, min_length=3)
     email = forms.EmailField(required=True, min_length=3)
-    
+
     class Meta:
         model = User
         fields = (
--- source/contact/views/user_forms.py
+++ source/contact/views/user_forms.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render
+
 from contact.forms import RegisterForm
 
 
@@ -10,5 +12,7 @@ def register(request):
 
         if form.is_valid():
             form.save()
+            messages.success(request, "Usuário registrado com sucesso!")
+            return redirect("contact:index")
 
     return render(request, "contact/register.html", {"form": form})
